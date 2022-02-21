f08ab5c7dee10c264241b1e348cfd21254175665
Ford Henley <fordp86@gmail.com>
2022-02-21 11:56:38 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+//alert("It's working!");
+
+// var welcome = "Hello There!";
+// console.log(welcome);
+
+// let greeting = "Hello Human!";
+// greeting = "Good Morning!";
+// console.log(greeting);
+
+// const constant = "I am a constant variable!";
+// console.log(constant);
+
+function sayHello(){
+    console.log("Hello!");
+}
